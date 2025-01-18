56785c17821a19b543c488796a8d2a46b1b503a3
Natsu N <merrysheep18@gmail.com>
2025-01-18 21:58:23 +0900

    タイトル一覧を追加する

--- memo-app-by-ts/src/index.ts
+++ memo-app-by-ts/src/index.ts
@@ -8,6 +8,11 @@ let memos: Memo[] = [];
 let memoIndex: number = 0;
 
 // ************************************************************
+// 要素一覧
+// ************************************************************
+const memoList = document.getElementById("list") as HTMLDivElement;
+
+// ************************************************************
 // 関数一覧
 // ************************************************************
 /**
@@ -25,6 +30,9 @@ function newMemo(): Memo {
   };
 }
 
+/**
+ * 初期化
+ */
 function init() {
   //ローカルストレージからすべてのメモを取得する
   memos = readLocalStorage(STORAGE_KEY);
@@ -34,7 +42,41 @@ function init() {
     //すべてのメモをローカルストレージに保存する
     saveLocalStorage(STORAGE_KEY, memos);
   }
-  console.log(memos);
+  showMemoElements(memoList, memos);
+}
+
+/**
+ * メモを追加する
+ * @param {Memo} memo メモ
+ * @returns {HTMLDivElementHTMLDivElement}
+ */
+function newMemoElement(memo: Memo): HTMLDivElement {
+  const div = document.createElement("div");
+  div.innerText = memo.title;
+  div.setAttribute("data-id", memo.id);
+  div.classList.add("w-full", "p-sm");
+  return div;
+}
+
+/**
+ * メモを削除する
+ * @param {HTMLDivElement} div メモ一覧のdiv要素
+ */
+function clearMemoElements(div: HTMLDivElement) {
+  div.innerText = "";
+}
+
+/**
+ * すべてのメモ要素を表示する
+ * @param {HTMLDivElement} div メモ一覧のdiv要素
+ * @param {Memo[]} memos メモ
+ */
+function showMemoElements(div: HTMLDivElement, memos: Memo[]) {
+  clearMemoElements(div);
+  memos.forEach(memo => {
+    const memoElement = newMemoElement(memo);
+    div.appendChild(memoElement);
+  });
 }
 
 init();
